072714f71d8b43f47c6e75e29c35c7f1ebee68e1
Johan Swan <jswannieman@gmail.com>
2024-02-06 12:25:47 +1300

    Commit javascript02 JS Component

--- javascript02.js
+++ javascript02.js
@@ -14,13 +14,40 @@ const retrieveData = (data) => {
   });
 };
 
+let result1Performed = false;
+
 button1.onclick = () => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      retrieveData(data);
-    })
-    .catch((error) => {
-      console.log(error);
+  if (!result1Performed) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        retrieveData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  result1Performed = true;
+};
+
+let result2Performed = false;
+
+const performResult2 = () => {
+  const url = "json.json";
+  let xmlHR = new XMLHttpRequest();
+  xmlHR.open("GET", url);
+  xmlHR.responseType = "json";
+  xmlHR.onload = () => {
+    let data = xmlHR.response;
+    data.sports.forEach((item) => {
+      output2.innerHTML += `<p>${item.sportName} has ${item.members} members and has it's origin since the year ${item.sinceYear}</p>`;
     });
+  };
+  xmlHR.send();
+  console.log(xmlHR);
+  result2Performed = true;
+};
+
+button2.onclick = () => {
+  if (!result2Performed) performResult2();
 };
